75ba817c368c7cdd6522381a76a5ac568020741d
valery kalombo <valerykalombo@hotmail.fr>
2021-06-21 20:22:03 +0200

    add image file

--- sneakers/resources/views/admin/index.blade.php
+++ sneakers/resources/views/admin/index.blade.php
@@ -1,5 +1,34 @@
 @extends('admin.template')
 
 @section('mycontent')
-    Hello World!
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                  <h5 class="card-title">Hommes</h5>
+                </div>
+                <img src="" class="card-img-bottom" alt="...">
+              </div>
+        </div>
+        
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                  <h5 class="card-title">Femmes</h5>
+                </div>
+                <img src="..." class="card-img-bottom" alt="...">
+              </div>
+        </div>
+        
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                  <h5 class="card-title">Enfants</h5>
+                </div>
+                <img src="..." class="card-img-bottom" alt="...">
+              </div>
+        </div>
+    </div>
+
 @endsection                         
