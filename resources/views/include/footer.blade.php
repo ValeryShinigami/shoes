4fd75a26e50a72a72103fbdac045016b564d9454
valery kalombo <valerykalombo@hotmail.fr>
2021-09-20 15:10:54 +0200

    cache reseau sociaux

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -15,11 +15,11 @@
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <p class="col-md-4 mb-0 text-muted">&copy; 2021 KALOMBO Valéry, Dev</p>
       
-          <a href="#" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto">
+          <!--<a href="#" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto">
             <img class="facebook" src="https://img.icons8.com/color/48/000000/facebook-new.png"/>
             <img class="insta" src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/>
             <img class="snap" src="https://img.icons8.com/color/48/000000/snapchat-circled-logo--v1.png"/>
-          </a>
+          </a>-->
       
           <ul class="nav col-md-4 justify-content-end">
             <li class="nav-item"><a href="#" class="nav-link px-2">Haut de page</a></li>
